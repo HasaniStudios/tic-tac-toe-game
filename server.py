c103abec68ca0592c7cf6f51d67e3c12f67423c6
Isaac Reaves <56567184+HasaniStudios@users.noreply.github.com>
2024-10-06 21:23:17 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -1,31 +1,24 @@
-# multiconn-server.py: a multi-connection ECHO server using the SELECT mechanism
-
 import sys
 import socket
 import selectors
 import types
 
 sel = selectors.DefaultSelector()
-
-# this routine is called when the LISTENING SOCKET gets a
-# connection request from a new client
+Players = []
 
 def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print("accepted connection from", addr)
-    conn.setblocking(False)
+    conn, addr = sock.accept()  
+    print("accepted connection from", addr, "this user will be considered player 1")
+    conn.settimeout(10)
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
     sel.register(conn, events, data=data)
-
-
-# this routine is called when a client is ready to read or write data
 
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
     if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
+        recv_data = sock.recv(1024)
         if recv_data:
             data.outb += recv_data
         else:
@@ -38,22 +31,16 @@ def service_connection(key, mask):
             sent = sock.send(data.outb)  # Should be ready to write
             data.outb = data.outb[sent:]
 
-
-# main program: set up the host address and port; change them if you need to
-
-host = '127.0.0.1'  # listens to any available IP address.  You might want to use 0.0.0.0 for the real internet
-port = 12358      # fibonacci numbers are cool;  irrelevant to this program, but still cool
-
-# set up the listening socket and register it with the SELECT mechanism
+host = '127.0.0.1'
+port = 5050
 
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
-print("listening on", (host, port))
-lsock.setblocking(False)
+print("This server is listening for players to connect", (host, port))
+lsock.settimeout(10)
 sel.register(lsock, selectors.EVENT_READ, data=None)
 
-# the main event loop
 try:
     while True:
         events = sel.select(timeout=None)
